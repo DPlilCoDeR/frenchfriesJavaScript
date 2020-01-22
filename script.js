e5cc5189ed870a4b4473fff1bbe8b7fdf75f75b2
Jason Carvajal Perez <jasonbnick@gmail.com>
2020-01-22 02:00:19 +0100

    crecion de funcion freir falta temporalizar el cambio del estado

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var papas = {
     cantidad:0,
     cortadas:false,
-    estado:"",
+    estado:["crudo","Hecho","quemado"],
 };
 console.log(papas);
 
@@ -13,10 +13,18 @@ function cuantasPapas() {
 
 function cortarPapas() {
     if (papas.cantidad === 0) {
-        alert("no puedes cortar lo que no tienes :(");
+        alert("no puedes cortar lo que no tienes");
         return;
     }
     papas.cortadas = true;
     return console.log(papas.cortadas);
 }
 
+function freirPapas() {
+    if (papas.cortadas == false) {
+        alert("no puedes freirlas si estan enteras");
+        return;
+    }
+    setInterval(freirPapas(),5000);
+    return;
+}
